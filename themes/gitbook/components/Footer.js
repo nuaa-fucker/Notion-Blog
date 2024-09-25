ece40fd446aa3181fb7b73513f701e7af43049d8
B3nDan <nuaa520@nuaa.edu.cn>
2024-09-26 00:39:36 +0800

    update

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -14,20 +14,6 @@ const Footer = ({ siteInfo }) => {
 
   return (
     <footer className='z-20 border p-2 rounded-lg bg:white dark:border-black dark:bg-hexo-black-gray justify-center text-center w-full text-sm relative'>
-      <SocialButton />
-
-      <div className='flex justify-center'>
-        <div>
-          <i className='mx-1 animate-pulse fas fa-heart' />{' '}
-          <a
-            href={siteConfig('LINK')}
-            className='underline font-bold text-gray-500 dark:text-gray-300 '>
-            {siteConfig('AUTHOR')}
-          </a>
-          .<br />
-        </div>
-        © {`${copyrightDate}`}
-      </div>
 
       {siteConfig('BEI_AN') && (
         <>
