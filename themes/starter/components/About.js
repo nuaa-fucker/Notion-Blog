bc479351efa92f48136ad1f76885f6cf8787c460
B3nDan <nuaa520@nuaa.edu.cn>
2024-11-12 08:23:29 +0800

    Update About.js

--- themes/starter/components/About.js
+++ themes/starter/components/About.js
@@ -27,11 +27,6 @@ export const About = () => {
                       __html: siteConfig('STARTER_ABOUT_TEXT')
                     }}></p>
 
-                  <a
-                    href={siteConfig('STARTER_ABOUT_BUTTON_URL')}
-                    className='inline-flex items-center justify-center rounded-md border border-primary bg-primary px-7 py-3 text-center text-base font-medium text-white hover:border-blue-dark hover:bg-blue-dark'>
-                    {siteConfig('STARTER_ABOUT_BUTTON_TEXT')}
-                  </a>
                 </div>
               </div>
 
